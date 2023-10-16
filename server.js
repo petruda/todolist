51880f852c597a632a7f6dd0474de741fa4a7aa1
petruda <petru650@gmail.com>
2023-10-16 14:50:28 +0300

    To do list webapp, in 90's brutalist website style, build 1.0

---
+++ server.js
@@ -0,0 +1,68 @@
+// express for making backend app
+import express from "express";
+//bodyparser to check the entries, as middlewear for stuff
+import bodyParser from "body-parser";
+//dirname to be able to use the path on a server as well
+import { dirname } from "path";
+// to take the url of the root
+import { fileURLToPath } from "url";
+// to make the full fath ( root + localpath)
+var todayItems = [];
+var workItems = []
+const __dirname = dirname(fileURLToPath(import.meta.url));
+//what port the server uses
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs", {
+        itemsT: todayItems,
+        itemG : "",
+    });
+});
+
+app.post("/", (req,res)=>{
+    var itemNameG = req.body["forg"];
+    res.render("index.ejs",{
+        itemsT: todayItems,
+        itemG : itemNameG,
+    });
+    todayItems.push(itemNameG);
+    console.log(todayItems.length);
+});
+
+
+
+app.get("/index.ejs", (req,res)=>{
+    var itemNameG = req.body["forg"];
+    res.render("index.ejs",{
+        itemsT: todayItems,
+        itemG : itemNameG,
+    })
+});
+
+app.get("/worklist.ejs", (req, res)=>{
+    res.render("worklist.ejs", {
+        itemW : "",
+        itemsW : workItems,
+    });
+});
+
+app.post("/submitw", (req,res)=>{
+    var itemNameW = req.body["forw"];
+    workItems.push (itemNameW);
+    res.render("worklist.ejs",{
+        itemsW : workItems,
+        itemW : itemNameW,
+    });
+    todayItems.push(itemNameW);
+    console.log(workItems.length);
+});
+
+//server start and logging the port for safety
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
